365f95db7d73902b860981d017f7ddc448265c9a
Pedro Lavôr <thiagolavor33@gmail.com>
2026-08-03 23:04:23 -0400

    feat: update bronze etl to process individual table

--- etl/etl_bronze.py
+++ etl/etl_bronze.py
@@ -7,25 +7,45 @@ from google.cloud import bigquery
 from datetime import datetime, timezone
 
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Set Argument Parser
+# Argument Parser
 parser = argparse.ArgumentParser(description="ETL Bronze Params")
 parser.add_argument('--gcp-billing-project-id', type=str, help='GCP Billing Project ID. env(GCP_BILLING_PROJECT_ID)')
 parser.add_argument('--bucket-bronze', type=str, help='Bronze Bucket URI. env(BUCKET_BRONZE)')
 parser.add_argument('--log-level', type=str, help='Log level. env(LOG_LEVEL)')
 parser.add_argument('--env', type=str, help='Environment. env(ENV)')
+parser.add_argument('--table', type=str, help='Table to extract. env(TABLE)')
 args = parser.parse_args()
 
 # Params Initialization
+load_dotenv()
 GCP_BILLING_PROJECT_ID = args.gcp_billing_project_id or os.getenv('GCP_BILLING_PROJECT_ID')
 BUCKET_BRONZE = args.bucket_bronze or os.getenv('BUCKET_BRONZE')
 LOG_LEVEL = args.log_level or os.getenv('LOG_LEVEL', 'INFO')
 ENV = args.env or os.getenv('ENV')
+TABLE = args.table or os.getenv('TABLE')
 
-if not GCP_BILLING_PROJECT_ID or not BUCKET_BRONZE:
-    raise SystemError("GCP_BILLING_PROJECT_ID and BUCKET_BRONZE must be provided either as command-line arguments or environment variables.")
+if not GCP_BILLING_PROJECT_ID:
+    raise SystemError("GCP_BILLING_PROJECT_ID must be provided either as a command-line argument or an environment variable.")
+
+if not BUCKET_BRONZE:
+    raise SystemError("BUCKET_BRONZE must be provided either as a command-line argument or an environment variable.")
+
+if not TABLE:
+    raise SystemError("TABLE must be provided either as a command-line argument or an environment variable.")
+
+# Variables
+TABLES = [
+    {"name": "uf", "path": "basedosdados.br_inep_avaliacao_alfabetizacao.uf"},
+    {"name": "municipio", "path": "basedosdados.br_inep_avaliacao_alfabetizacao.municipio"},
+    {"name": "alunos", "path": "basedosdados.br_inep_avaliacao_alfabetizacao.alunos"},
+    {"name": "dicionario", "path": "basedosdados.br_inep_avaliacao_alfabetizacao.dicionario"},
+    {"name": "meta_alfabetizacao_brasil", "path": "basedosdados.br_inep_avaliacao_alfabetizacao.meta_alfabetizacao_brasil"},
+    {"name": "meta_alfabetizacao_municipio", "path": "basedosdados.br_inep_avaliacao_alfabetizacao.meta_alfabetizacao_municipio"},
+    {"name": "meta_alfabetizacao_uf", "path": "basedosdados.br_inep_avaliacao_alfabetizacao.meta_alfabetizacao_uf"}
+]
+
+if TABLE not in map(lambda x: x["name"], TABLES):
+    raise SystemError("TABLE must be one of the tables in TABLES.")
 
 # Logger
 logging.basicConfig(
@@ -34,69 +54,61 @@ logging.basicConfig(
     datefmt="%Y-%m-%dT%H:%M:%SZ",
 )
 log = logging.getLogger(__name__)
-log.debug(f"LOG_LEVEL: {LOG_LEVEL}, ENV: {ENV}, GCP_BILLING_PROJECT_ID: {GCP_BILLING_PROJECT_ID}, BUCKET_BRONZE: {BUCKET_BRONZE}")
-
-# Variables
-NOW = datetime.now(timezone.utc)
-TABLES = [
-    "basedosdados.br_inep_avaliacao_alfabetizacao.uf",
-    "basedosdados.br_inep_avaliacao_alfabetizacao.municipio",
-    "basedosdados.br_inep_avaliacao_alfabetizacao.alunos",
-    "basedosdados.br_inep_avaliacao_alfabetizacao.dicionario",
-    "basedosdados.br_inep_avaliacao_alfabetizacao.meta_alfabetizacao_brasil",
-    "basedosdados.br_inep_avaliacao_alfabetizacao.meta_alfabetizacao_municipio",
-    "basedosdados.br_inep_avaliacao_alfabetizacao.meta_alfabetizacao_uf"
-]
+log.debug(f"ENV: {ENV}, LOG_LEVEL: {LOG_LEVEL}, GCP_BILLING_PROJECT_ID: {GCP_BILLING_PROJECT_ID}, BUCKET_BRONZE: {BUCKET_BRONZE}, TABLE: {TABLE}")
 
 # Functions
-def extract(tables: list) -> dict[str, pd.DataFrame]:
-    """Extract data from BigQuery and cache it locally."""
-    bigquery_client = bigquery.Client(project=GCP_BILLING_PROJECT_ID)
-    dataframes = {}
+def extract(table: dict[str, str]) -> pd.DataFrame:
+    """Extract basedosdados table from BigQuery and cache locally if env local."""
+    table_name = table["name"]
+    table_path = table["path"]
+    
+    try:
+        dataframe = pd.DataFrame()
+        cache_path = f".data/.cache_extract/{table_name}.parquet"
 
-    for table in tables:
-        try:
+        if ENV == "local" and os.path.exists(cache_path):
+            log.debug(f"Loading data from local cache: {cache_path}")
+            dataframe = pd.read_parquet(cache_path)
 
-            df_pandas = None
-            cache_path = f".data/extract_cache/{table}.parquet"
+        else:
+            log.debug(f"Loading data from BigQuery: {table_path}")
+            bigquery_client = bigquery.Client(project=GCP_BILLING_PROJECT_ID)
+            dataframe = bigquery_client.list_rows(table_path).to_dataframe()
 
-            if ENV == "local" and os.path.exists(cache_path):
-                log.debug(f"Loading data from local cache: {cache_path}")
-                df_pandas = pd.read_parquet(cache_path)
+            if ENV == "local":
+                os.makedirs(os.path.dirname(cache_path), exist_ok=True)
+                dataframe.to_parquet(cache_path, index=False)
+                log.debug(f"Cached data to local path: {cache_path}")
 
-            else:
-                log.debug(f"Loading data from BigQuery: {table}")
-                df_pandas = bigquery_client.list_rows(table).to_dataframe()
-                if ENV == "local":
-                    os.makedirs(os.path.dirname(cache_path), exist_ok=True)
-                    df_pandas.to_parquet(cache_path, index=False)
-                    log.debug(f"Cached data to local path: {cache_path}")
+        dataframe['_env'] = ENV
+        dataframe['_source_table'] = table_path
+        dataframe['_ingested_at'] = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+        log.debug(f"Extracted {len(dataframe)} rows from table: {table_path}")
+        return dataframe
 
-            df_pandas['_env'] = ENV
-            df_pandas['_table_id'] = table
-            df_pandas['_ingested_at'] = NOW.strftime("%Y-%m-%dT%H:%M:%SZ")
-            dataframes[table] = df_pandas
-            log.debug(f"Extracted {df_pandas.shape[0]} rows from table: {table}")
-
-        except Exception as e:
-            log.error(f"Error extracting data from table {table}: {e}")
-            raise e
-        
-    return dataframes
+    except Exception as e:
+        log.error(f"Error extracting data from table {table_path}: {e}")
+        raise e
 
 
-def load(dataframes: dict[str, pd.DataFrame]):
-    """Load the extracted data into the target storage."""
-    for table, df in dataframes.items():
-        df.to_parquet(f"{BUCKET_BRONZE}/{table}.parquet", index=False)
-        log.debug(f"Data loaded for table: {table}")
-
+def load(table: dict[str, str], dataframe: pd.DataFrame):
+    """Load dataframe to bronze bucket."""
+    file_path = f"{BUCKET_BRONZE}/{table['name']}.parquet"
+    try:
+        dataframe.to_parquet(file_path, index=False)
+        log.debug(f"Dataframe loaded on {file_path}")
+    except Exception as e:
+        log.error(f"Error loading dataframe to {file_path}: {e}")
+        raise e
 
 # Execution
 if __name__ == "__main__":
     log.info(f"Starting ETL Bronze process...")
     init_time = datetime.now()
-    extracted_data = extract(TABLES)
-    load(extracted_data)
+
+    table = next((t for t in TABLES if t["name"] == TABLE), None)
+    dataframe = extract(table)
+    load(table, dataframe)
+
     elapsed_time = datetime.now() - init_time
-    log.info(f"ETL Bronze process completed. Extracted data for {len(extracted_data)} tables in {elapsed_time.total_seconds():.1f} seconds.")
+    log.info(f"ETL Bronze process completed. Extracted data for {len(dataframe)} tables in {elapsed_time.total_seconds():.1f} seconds.")
