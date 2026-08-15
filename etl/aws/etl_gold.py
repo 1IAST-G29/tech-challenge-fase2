41bc3450ae0ee7c905416ba81e9045abf7026e1b
Pedro Lavôr <thiagolavor33@gmail.com>
2026-08-15 18:57:52 -0400

    feat: add aws etl jobs and update silver/gold pipeline

--- etl/aws/etl_gold.py
+++ etl/aws/etl_gold.py
@@ -1,64 +1,52 @@
 import os
-import argparse
+import sys
 import logging
 import pandas as pd
-from dotenv import load_dotenv
 from datetime import datetime, timezone
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
 
 
-# Load environment variables from .env file
-load_dotenv()
+## @params: [JOB_NAME, GCP_BILLING_PROJECT_ID, BUCKET_BRONZE, LOG_LEVEL, ENV, TABLE]
+args = getResolvedOptions(sys.argv, [
+    'JOB_NAME',
+    'BUCKET_SILVER',
+    'BUCKET_GOLD',
+    'LOG_LEVEL',
+    'ENV',
+])
 
-# Set Argument Parser
-parser = argparse.ArgumentParser(description="ETL Bronze Params")
-parser.add_argument('--gcp-billing-project-id', type=str, help='GCP Billing Project ID. env(GCP_BILLING_PROJECT_ID)')
-parser.add_argument('--bucket-silver', type=str, help='Silver Bucket URI. env(BUCKET_SILVER)')
-parser.add_argument('--bucket-gold', type=str, help='Gold Bucket URI. env(BUCKET_GOLD)')
-parser.add_argument('--log-level', type=str, help='Log level. env(LOG_LEVEL)')
-parser.add_argument('--env', type=str, help='Environment. env(ENV)')
-parser.add_argument('--table', type=str, help='Table to extract. env(TABLE)')
-args = parser.parse_args()
 
 # Params Initialization
-GCP_BILLING_PROJECT_ID = args.gcp_billing_project_id or os.getenv('GCP_BILLING_PROJECT_ID')
-BUCKET_SILVER = args.bucket_silver or os.getenv('BUCKET_SILVER')
-BUCKET_GOLD = args.bucket_gold or os.getenv('BUCKET_GOLD')
-LOG_LEVEL = args.log_level or os.getenv('LOG_LEVEL', 'INFO')
-ENV = args.env or os.getenv('ENV')
-TABLE = args.table or os.getenv('TABLE')
+JOB_NAME = args['JOB_NAME']
+BUCKET_SILVER = args['BUCKET_SILVER']
+BUCKET_GOLD = args['BUCKET_GOLD']
+LOG_LEVEL = args['LOG_LEVEL']
+ENV = args['ENV']
 
-if not GCP_BILLING_PROJECT_ID:
-    raise SystemError("GCP_BILLING_PROJECT_ID must be provided either as a command-line argument or an environment variable.")
-
-if not BUCKET_SILVER:
-    raise SystemError("BUCKET_SILVER must be provided either as a command-line argument or an environment variable.")
-
-if not BUCKET_GOLD:
-    raise SystemError("BUCKET_GOLD must be provided either as a command-line argument or an environment variable.")
-
-if not TABLE:
-    raise SystemError("TABLE must be provided either as a command-line argument or an environment variable.")
-
-# Logger
 logging.basicConfig(
     level=LOG_LEVEL.upper(),
     format="%(asctime)s | %(levelname)-8s | %(message)s",
     datefmt="%Y-%m-%dT%H:%M:%SZ",
 )
 log = logging.getLogger(__name__)
-log.debug(f"ENV: {ENV}, LOG_LEVEL: {LOG_LEVEL}, GCP_BILLING_PROJECT_ID: {GCP_BILLING_PROJECT_ID}, BUCKET_SILVER: {BUCKET_SILVER}, BUCKET_GOLD: {BUCKET_GOLD}, TABLE: {TABLE}")
+
+# Params Initialization
+if not BUCKET_SILVER:
+    raise SystemError("BUCKET_SILVER must be provided either as a command-line argument or an environment variable.")
+
+if not BUCKET_GOLD:
+    raise SystemError("BUCKET_GOLD must be provided either as a command-line argument or an environment variable.")
+
+
+# Logger
+log.debug(f"ENV: {ENV}, LOG_LEVEL: {LOG_LEVEL}, BUCKET_SILVER: {BUCKET_SILVER}, BUCKET_GOLD: {BUCKET_GOLD}")
 
 # Variables
 NOW = datetime.now(timezone.utc)
-TABLES = [
-    "basedosdados.br_inep_avaliacao_alfabetizacao.uf",
-    "basedosdados.br_inep_avaliacao_alfabetizacao.municipio",
-    "basedosdados.br_inep_avaliacao_alfabetizacao.alunos",
-    "basedosdados.br_inep_avaliacao_alfabetizacao.dicionario",
-    "basedosdados.br_inep_avaliacao_alfabetizacao.meta_alfabetizacao_brasil",
-    "basedosdados.br_inep_avaliacao_alfabetizacao.meta_alfabetizacao_municipio",
-    "basedosdados.br_inep_avaliacao_alfabetizacao.meta_alfabetizacao_uf"
-]
 
 def build_brazil_literacy_result():
     df_raw = pd.read_parquet(f"{BUCKET_SILVER}/meta_alfabetizacao_brasil.parquet")
@@ -163,4 +151,4 @@ if __name__ == "__main__":
     build_students_result()
 
     elapsed_time = datetime.now() - init_time
-    log.info(f"ETL Gold process completed. Extracted data for {len(TABLES)} tables in {elapsed_time.total_seconds():.1f} seconds.")
+    log.info(f"ETL Gold process completed. Processed data in {elapsed_time.total_seconds():.1f} seconds.")
